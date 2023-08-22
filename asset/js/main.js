c6e64069d7cd83d9530e4982937ebc40a7bf309e
FabioVasi <vasifabio96@gmail.com>
2023-08-22 15:40:25 +0200

    Add prompt

--- asset/js/main.js
+++ asset/js/main.js
@@ -8,15 +8,32 @@ che va formattato in questo modo: nomecognomecolorepreferito23.
 Strumenti.
 
 - prompt
-- let userAge
+- let userName
+- let userSurname
+- let userColor
 - console.log
 - document.getElementById
 
 */
 
 // Chiedere il nome dell'utente
+
+const userName = prompt('inserisci il tuo nome');
+
 // Chiedere il cognome dell'utente
+
+const userSurame = prompt('inserisci il tuo cognome');
+
 // Chiedere il colore preferito dell'utente
+
+const userColor = prompt('inserisci il tuo colore preferito');
+
 // Stampare il risultato in console
+
+
+
 // Stampare il risultato in pagina
+
+
+
 // Formattare il risultato nel seguente modo: nomecognomecolorepreferito23
