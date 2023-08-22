c5492da922372251a39f714fe4adb105c4b11473
FabioVasi <vasifabio96@gmail.com>
2023-08-22 15:32:43 +0200

    Add Pseudo code

---
+++ asset/js/main.js
@@ -0,0 +1,22 @@
+/*
+Chiedi all’utente il suo nome, poi chiedi il suo cognome, poi chiedi il suo colore preferito ed infine scrivi in pagina (non solo in console) il risultato,
+che va formattato in questo modo: nomecognomecolorepreferito23.
+*/
+
+/*
+
+Strumenti.
+
+- prompt
+- let userAge
+- console.log
+- document.getElementById
+
+*/
+
+// Chiedere il nome dell'utente
+// Chiedere il cognome dell'utente
+// Chiedere il colore preferito dell'utente
+// Stampare il risultato in console
+// Stampare il risultato in pagina
+// Formattare il risultato nel seguente modo: nomecognomecolorepreferito23
